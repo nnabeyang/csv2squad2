85c3a35bb27426dc372fed8a0130a16655e2d626
nnabeyang <nabeyang@gmail.com>
2021-09-25 09:35:17 +0900

    Fix conversion for having more than 2 kind of titles in input file

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/tobgu/qframe v0.3.6-0.20210408204126-9d7de1fa1662
 )
--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 	"io"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
-	"unicode/utf8"
+
+	"github.com/google/uuid"
 
 	"github.com/gocarina/gocsv"
 
@@ -31,6 +31,8 @@ type CsvRow struct {
 	Answer       string `csv:"answer"`
 	AnswerStart  int64  `csv:"answer_start"`
 	IsImpossible bool   `csv:"is_impossible"`
+	ID           string `csv:"id"`
+	Title        string `csv:"title"`
 }
 
 func (row *CsvRow) copy() *CsvRow {
@@ -40,6 +42,8 @@ func (row *CsvRow) copy() *CsvRow {
 		Answer:       row.Answer,
 		AnswerStart:  row.AnswerStart,
 		IsImpossible: row.IsImpossible,
+		ID:           row.ID,
+		Title:        row.Title,
 	}
 }
 
@@ -52,23 +56,62 @@ type QnA struct {
 	ID               string    `json:"id"`
 	Question         string    `json:"question"`
 	Answers          []*Answer `json:"answers"`
-	PlausibleAnswers []*Answer `json:"plausible_answers,omitempty"`
+	PlausibleAnswers []*Answer `json:"-"`
 	IsImpossible     bool      `json:"is_impossible"`
 }
 
+func (q *QnA) MarshalJSON() ([]byte, error) {
+	type Alias QnA
+
+	if q.IsImpossible {
+		return json.Marshal(&struct {
+			*Alias
+			AliasPlausibleAnswers []*Answer `json:"plausible_answers"`
+		}{
+			Alias:                 (*Alias)(q),
+			AliasPlausibleAnswers: q.PlausibleAnswers,
+		})
+	} else {
+		return json.Marshal((*Alias)(q))
+	}
+}
+
+func (q *QnA) UnmarshalJSON(b []byte) error {
+	type Alias QnA
+
+	aux := &struct {
+		*Alias
+		AliasPlausibleAnswers []*Answer `json:"plausible_answers"`
+	}{
+		Alias: (*Alias)(q),
+	}
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+
+	q.PlausibleAnswers = aux.AliasPlausibleAnswers
+	return nil
+}
 func (q *QnA) toCsvRows(row *CsvRow) []*CsvRow {
-	row.Question = toASCII(q.Question)
+	row.Question = q.Question
 	row.IsImpossible = q.IsImpossible
+	row.ID = q.ID
 	var answers []*Answer
 	if q.IsImpossible {
 		answers = q.PlausibleAnswers
 	} else {
 		answers = q.Answers
 	}
+	if len(answers) == 0 {
+		r := row.copy()
+		r.Answer = ""
+		r.AnswerStart = -1
+		return []*CsvRow{row}
+	}
 	rows := make([]*CsvRow, len(answers))
 	for i, answer := range answers {
 		r := row.copy()
-		r.Answer = toASCII(answer.Text)
+		r.Answer = answer.Text
 		r.AnswerStart = answer.AnswerStart
 		rows[i] = r
 	}
@@ -80,9 +123,10 @@ type Paragraph struct {
 	Qas     []*QnA `json:"qas"`
 }
 
-func (p *Paragraph) toCsvRows() []*CsvRow {
+func (p *Paragraph) toCsvRows(title string) []*CsvRow {
 	row := &CsvRow{
-		Context: toASCII(p.Context),
+		Context: p.Context,
+		Title:   title,
 	}
 	rows := make([]*CsvRow, 0, len(p.Qas)*10)
 	for _, qna := range p.Qas {
@@ -100,7 +144,7 @@ type SuQUADData struct {
 func (d *SuQUADData) toCsvRows() []*CsvRow {
 	rows := make([]*CsvRow, 0, len(d.Paragraphs)*100)
 	for _, p := range d.Paragraphs {
-		rows = append(rows, p.toCsvRows()...)
+		rows = append(rows, p.toCsvRows(d.Title)...)
 	}
 	return rows
 }
@@ -121,16 +165,34 @@ func (s *SuQUAD) ToCsv(w io.Writer) {
 type AnswerGroup struct {
 	Answers      []*Answer
 	IsImpossible bool
+	ID           string
+}
+type ParagraphGroup struct {
+	Paragraphs []*Paragraph
+	Title      string
 }
 type Env struct {
-	Title        string
-	Answers      map[string]*Answer
-	AnswerGroups map[string]*AnswerGroup
-	QAs          map[string]*QnA
-	QAGroups     map[string][]*QnA
-	Paragraphs   []*Paragraph
+	ContextIndex    int
+	TitleIndex      int
+	Answers         map[string]*Answer
+	AnswerGroups    map[string]*AnswerGroup
+	QAs             map[string]*QnA
+	QAGroups        map[string][]*QnA
+	Paragraphs      map[string]*Paragraph
+	ParagraphGroups map[string]*SuQUADData
+	Data            []*SuQUADData
 }
 
+func (o *Env) contextIndex(a *string) int {
+	idx := o.ContextIndex
+	o.ContextIndex++
+	return idx
+}
+func (o *Env) titleIndex(a *string) int {
+	idx := o.TitleIndex
+	o.TitleIndex++
+	return idx
+}
 func (o *Env) createAnswer(a *string, b *string) *string {
 	if b == nil {
 		return nil
@@ -139,15 +201,14 @@ func (o *Env) createAnswer(a *string, b *string) *string {
 	if err != nil {
 		return nil
 	}
+
 	s := Answer{
 		Text:        *b,
 		AnswerStart: idx,
 	}
-	buf, err := json.Marshal(&s)
-	if err != nil {
-		return nil
-	}
-	id := fmt.Sprintf("%x", md5.Sum(buf))
+
+	u, _ := uuid.NewRandom()
+	id := u.String()
 	o.Answers[id] = &s
 	return &id
 }
@@ -164,7 +225,7 @@ func (o *Env) groupAnswers(answer_ids []*string) *string {
 
 	}
 
-	b, err := json.Marshal(&a)
+	b, err := json.Marshal(&answer_ids)
 	if err != nil {
 		return nil
 	}
@@ -173,9 +234,22 @@ func (o *Env) groupAnswers(answer_ids []*string) *string {
 	return &id
 }
 
+func (o *Env) groupContextIdx(indices []int) int {
+	return indices[0]
+}
 func (o *Env) addIsImpossible(a *string, b *string) *string {
 	group := o.AnswerGroups[*b]
 	group.IsImpossible = *a == "true"
+	return b
+}
+
+func (o *Env) addID(a *string, b *string) *string {
+	group := o.AnswerGroups[*b]
+	if strings.TrimSpace(*a) == "" {
+		group.ID = *b
+	} else {
+		group.ID = *a
+	}
 	return b
 }
 
@@ -183,16 +257,22 @@ func (o *Env) collectQAs(answerID *string, b *string) *string {
 	if ag, ok := o.AnswerGroups[*answerID]; ok {
 		var s QnA
 		if ag.IsImpossible {
+			answers := make([]*Answer, 0, len(ag.Answers))
+			for _, answer := range ag.Answers {
+				if strings.TrimSpace(answer.Text) != "" {
+					answers = append(answers, answer)
+				}
+			}
 			s = QnA{
-				ID:               *answerID,
+				ID:               ag.ID,
 				Question:         *b,
-				PlausibleAnswers: ag.Answers,
+				PlausibleAnswers: answers,
 				Answers:          make([]*Answer, 0),
 				IsImpossible:     ag.IsImpossible,
 			}
 		} else {
 			s = QnA{
-				ID:           *answerID,
+				ID:           ag.ID,
 				Question:     *b,
 				Answers:      ag.Answers,
 				IsImpossible: ag.IsImpossible,
@@ -231,19 +311,39 @@ func (o *Env) makeParagraph(answerID *string, context *string) *string {
 			Context: *context,
 			Qas:     qas,
 		}
-		o.Paragraphs = append(o.Paragraphs, &s)
+		u, _ := uuid.NewRandom()
+		id := u.String()
+		o.Paragraphs[id] = &s
+		return &id
+	}
+	return nil
+}
+
+func (o *Env) groupParagraphs(ids []*string) *string {
+	a := make([]*Paragraph, 0, len(ids))
+	for _, id := range ids {
+		if v, ok := o.Paragraphs[*id]; ok {
+			a = append(a, v)
+		}
+	}
+	u, _ := uuid.NewRandom()
+	id := u.String()
+	o.ParagraphGroups[id] = &SuQUADData{Paragraphs: a}
+	return &id
+}
+
+func (o *Env) makeData(paraID *string, title *string) *string {
+	if group, ok := o.ParagraphGroups[*paraID]; ok {
+		group.Title = *title
+		o.Data = append(o.Data, group)
 	}
 	return nil
 }
 
 func (o *Env) ToJSON(w io.Writer) error {
-	data := &SuQUADData{
-		Title:      o.Title,
-		Paragraphs: o.Paragraphs,
-	}
 	s := SuQUAD{
 		Version: "v2.0",
-		Data:    []*SuQUADData{data},
+		Data:    o.Data,
 	}
 	b := new(bytes.Buffer)
 	enc := json.NewEncoder(b)
@@ -255,61 +355,18 @@ func (o *Env) ToJSON(w io.Writer) error {
 	return err
 }
 
-func NewEnv(title string) *Env {
+func NewEnv() *Env {
 	return &Env{
-		Title:        toASCII(title),
-		Answers:      make(map[string]*Answer),
-		QAs:          make(map[string]*QnA),
-		AnswerGroups: make(map[string]*AnswerGroup),
-		QAGroups:     make(map[string][]*QnA),
-		Paragraphs:   make([]*Paragraph, 0),
+		TitleIndex:      0,
+		ContextIndex:    0,
+		Answers:         make(map[string]*Answer),
+		QAs:             make(map[string]*QnA),
+		AnswerGroups:    make(map[string]*AnswerGroup),
+		QAGroups:        make(map[string][]*QnA),
+		Paragraphs:      make(map[string]*Paragraph),
+		ParagraphGroups: make(map[string]*SuQUADData),
+		Data:            make([]*SuQUADData, 0),
 	}
-}
-
-func toASCII(s string) string {
-	q := strconv.QuoteToASCII(s)
-	r := strings.ReplaceAll(q[1:len(q)-1], "\\\"", "\"")
-	return r
-}
-
-func ASCIIStr(s *string) *string {
-	re := regexp.MustCompile(`\\`)
-	inp := *s
-	idxs := re.FindAllIndex([]byte(inp), -1)
-	if len(idxs) == 0 {
-		return s
-	}
-	var runeTmp [utf8.UTFMax]byte
-	buf := make([]byte, 0, 3*len(inp)/2)
-	i := 0
-	for _, idx := range idxs {
-		buf = append(buf, inp[i:idx[0]]...)
-		var m int
-		switch inp[idx[0]+1] {
-		case 'x':
-			m = 2
-		case 'u':
-			m = 4
-		case 'U':
-			m = 8
-		default:
-			m = 0
-		}
-		if m == 0 {
-			i = idx[0]
-		} else {
-			c, _, _, err := strconv.UnquoteChar(inp[idx[0]:], '"')
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-			n := utf8.EncodeRune(runeTmp[:], c)
-			buf = append(buf, runeTmp[:n]...)
-			i = idx[0] + m + 2
-		}
-
-	}
-	o := toASCII(string(buf))
-	return &o
 }
 
 func main() {
@@ -317,20 +374,17 @@ func main() {
 	var inFilePath string
 	var outFilePath string
 	var reverseFlag bool
-	var title string
 	flag.BoolVar(&showVersion, "v", false, "show version")
 	flag.BoolVar(&showVersion, "version", false, "show version")
 	flag.BoolVar(&reverseFlag, "r", false, "reverse mode flag")
 	flag.StringVar(&inFilePath, "i", "", "infile path")
 	flag.StringVar(&outFilePath, "o", "", "outfile path")
-	flag.StringVar(&title, "t", "", "title field of outfile")
-	flag.StringVar(&title, "title", "", "title field of outfile")
 	flag.Parse()
 	if showVersion {
 		fmt.Println("version:", version)
 		return
 	}
-	if inFilePath == "" || (!reverseFlag && title == "") {
+	if inFilePath == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -359,24 +413,31 @@ func main() {
 		}
 		e.ToCsv(w)
 	} else {
-		env := NewEnv(title)
+		env := NewEnv()
 		qframe.ReadCSV(r).Drop("answer_check").Apply(
-			qframe.Instruction{Fn: ASCIIStr, DstCol: "context", SrcCol1: "context"},
-			qframe.Instruction{Fn: ASCIIStr, DstCol: "question", SrcCol1: "question"},
-			qframe.Instruction{Fn: ASCIIStr, DstCol: "answer", SrcCol1: "answer"},
+			qframe.Instruction{Fn: env.titleIndex, DstCol: "title_idx", SrcCol1: "title"},
+			qframe.Instruction{Fn: env.contextIndex, DstCol: "context_idx", SrcCol1: "context"},
 			qframe.Instruction{Fn: function.StrI, DstCol: "answer_start", SrcCol1: "answer_start"},
-		).Drop("frag_text").
-			Apply(qframe.Instruction{
+			qframe.Instruction{
 				Fn:      env.createAnswer,
 				SrcCol1: "answer_start",
 				SrcCol2: "answer",
 				DstCol:  "answer_id",
-			}).Drop("answer", "answer_start").
-			GroupBy(groupby.Columns("context", "question", "is_impossible")).
+			},
+		).Drop("frag_text", "answer", "answer_start").
+			GroupBy(groupby.Columns("context", "question", "is_impossible", "id", "title")).
 			Aggregate(qframe.Aggregation{
 				Fn:     env.groupAnswers,
 				Column: "answer_id",
 				As:     "answer_id",
+			}, qframe.Aggregation{
+				Fn:     env.groupContextIdx,
+				Column: "context_idx",
+				As:     "context_idx",
+			}, qframe.Aggregation{
+				Fn:     env.groupContextIdx,
+				Column: "title_idx",
+				As:     "title_idx",
 			}).
 			Apply(qframe.Instruction{
 				Fn:      function.StrB,
@@ -386,25 +447,59 @@ func main() {
 				Fn:      env.addIsImpossible,
 				SrcCol1: "is_impossible",
 				SrcCol2: "answer_id",
-				DstCol:  "answer_id",
-			}).
-			Sort(qframe.Order{Column: "question"}).
+				DstCol:  "dummy1",
+			}, qframe.Instruction{
+				Fn:      env.addID,
+				SrcCol1: "id",
+				SrcCol2: "answer_id",
+				DstCol:  "dummy2",
+			}).Drop("dummy1", "dummy2").
+			Sort(qframe.Order{Column: "title_idx"}, qframe.Order{Column: "id"}).
 			Apply(qframe.Instruction{
 				Fn:      env.collectQAs,
 				SrcCol1: "answer_id",
 				SrcCol2: "question",
-				DstCol:  "answer_id"},
+				DstCol:  "question_id"},
 			).
-			GroupBy(groupby.Columns("context")).
+			Drop("id", "answer_id").
+			GroupBy(groupby.Columns("context", "title")).
 			Aggregate(qframe.Aggregation{
 				Fn:     env.groupQAs,
-				Column: "answer_id",
-				As:     "answer_id",
-			}).Sort(qframe.Order{Column: "context"}).
+				Column: "question_id",
+				As:     "question_id",
+			}, qframe.Aggregation{
+				Fn:     env.groupContextIdx,
+				Column: "context_idx",
+				As:     "context_idx",
+			}, qframe.Aggregation{
+				Fn:     env.groupContextIdx,
+				Column: "title_idx",
+				As:     "title_idx",
+			}).
+			Sort(qframe.Order{Column: "context_idx"}).
+			Drop("context_idx2").
 			Apply(qframe.Instruction{
 				Fn:      env.makeParagraph,
-				SrcCol1: "answer_id",
+				SrcCol1: "question_id",
 				SrcCol2: "context",
+				DstCol:  "para_id"},
+			).
+			Drop("dummy").
+			GroupBy(groupby.Columns("title")).
+			Aggregate(qframe.Aggregation{
+				Fn:     env.groupParagraphs,
+				Column: "para_id",
+				As:     "para_id",
+			}, qframe.Aggregation{
+				Fn:     env.groupContextIdx,
+				Column: "title_idx",
+				As:     "title_idx",
+			}).
+			Sort(qframe.Order{Column: "title_idx"}).
+			Apply(qframe.Instruction{
+				Fn:      env.makeData,
+				SrcCol1: "para_id",
+				SrcCol2: "title",
 				DstCol:  "dummy"},
 			)
 		if err := env.ToJSON(w); err != nil {
